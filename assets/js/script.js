4c8ce643c515e04b74e5095f6abc04d1e969a39f
AdrienCodeVH <adriencodeprog@gmail.com>
2026-04-02 13:55:14 +0200

    feat: structure le site en multipages

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const infoButton = document.getElementById("infoButton");
+
+if (infoButton) {
+  infoButton.addEventListener("click", () => {
+    alert("Le site Bouguenais Ludique est en cours de construction.");
+  });
+}
